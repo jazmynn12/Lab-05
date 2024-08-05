e1bc1d4b39a1775fff177ceb4b95c2782618920c
mHal-r <89000258+mHal-r@users.noreply.github.com>
2024-08-04 19:04:35 -0700

    Update dollar.cpp

--- dollar.cpp
+++ dollar.cpp
@@ -2,6 +2,8 @@
  *Lab Number: 2
  *Name: Mira Haldar and Jazmyne Newman
  *This file defines the Dollar class, which inherits from Currency
+ Changes from Lab 2: 
+ Created a copy constructor inheriting from the one in the currency class
  */
 #include "currency.cpp"
 class Dollar : public Currency {
@@ -9,5 +11,7 @@ private:
     std::string currencyName = "Dollar"; 
 public: 
     Dollar(double amount) : Currency(amount) {} 
-    std::string getCurrencyName() override { return currencyName; } 
+    Dollar( const Currency & other ) : Currency(other) {}; 
+    
+    virtual std::string getCurrencyName() const { return currencyName; } 
 }; 
